bca1fd510a86f6ae2d61661715dbad15e03d5287
ADV475689 <78424494+ADV475689@users.noreply.github.com>
2021-05-18 17:08:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var surname="Paulo";
+console.log(surname);
+
+var actualname="Dybala";
+console.log(actualname)
+
+var fullname=surname.concat(actualname);
+console.log(fullname);
+
+var a="15";
+console.log(a);
+
+var b="10";
+console.log(b);
+
+var ab=["10","15"];
+console.log(ab);
+
+var sum= a+b;
+console.log(sum);
